49fa2e80070922fc0089d17e918c27c782713345
ebrandes <eduardocbrandes@gmail.com>
2014-05-23 01:44:53 -0300

    dalee, login responsivo jÃ¡

---
+++ application/views/incial.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Bem vindo ao TcheBooks</title>
+	<!--Estilos-->
+	<link href="/html/css/bootstrap.css" type="text/css" rel="stylesheet" />
+	<link href="/html/css/bootstrap-theme.css" type="text/css" rel="stylesheet" />
+	<link href="/html/css/default.css" type="text/css" rel="stylesheet" />
+	<!-- Scripts -->
+	<script type="text/javascript" src="/html/js/jquery.js"></script>
+	<script type="text/javascript" src="/html/js/bootstrap.js"></script>
+
+	<?php 
+	if(isset($css)){
+		Uteis::loadCss($css);
+	}
+	if(isset($js)) {
+		Uteis::loadJs($js);	
+	}
+	?>
+</head>
+<body>
+	<div class="container-fluid">
+
+		<div class="row tituloPrincipal">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<h1 class="text-center"><small class="bemVindo">Bem vindo ao</small> TCHEBooks.COM</h1>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+
+		<div class="row frmLogin">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<form role="form">
+					<div class="form-group">
+						<label for="login">Login</label>
+						<input type="email" class="form-control" id="login" placeholder="Utilize o login ou seu email">
+					</div>
+					<div class="form-group">
+						<label for="senha">Password</label>
+						<input type="password" class="form-control" id="senha" placeholder="Senha">
+					</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+					<a href="#" class="text-right">esqueci minha senha</a>
+				</form>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+
+	</div>
+</body>
+</html>
