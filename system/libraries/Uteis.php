a0102b44de47d5ceb7169c87f6e87fbad8316e23
ebrandes <eduardocbrandes@gmail.com>
2014-05-23 00:31:26 -0300

    Aleluia

---
+++ system/libraries/Uteis.php
@@ -0,0 +1,22 @@
+<?
+
+class Uteis {
+
+	
+	public function loadCss($arrayCss){
+
+		foreach ($arrayCss as $css) {
+			echo "<link rel='stylesheet' type='text/css' href='/html/css/$css' />"
+		}
+
+	}
+
+	public function loadJs($arrayJS){
+
+		foreach ($arrayJS as $js) {
+			echo "<script type='text/javascript' src='/html/js/$js'></script>";
+		}
+
+	}
+	
+}
