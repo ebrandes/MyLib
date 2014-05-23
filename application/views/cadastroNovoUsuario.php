69986eb94c90f20a7e8d3015fcaea6bf3fcd34e2
ebrandes <eduardocbrandes@gmail.com>
2014-05-23 02:50:23 -0300

    cadastro de usuÃ¡rio pronto

--- application/views/cadastroNovoUsuario.php
+++ application/views/cadastroNovoUsuario.php
@@ -4,26 +4,49 @@
 		<div class="col-md-4"></div>
 		<div class="col-md-4">
 			<h1 class="text-center tituloLogo">TCHEBooks.COM</h1>
-			<h2 class="text-center">Novo usuário</h1>
+			<h2 class="text-left subTitulo">Novo usuário</h1>
+			</div>
+			<div class="col-md-4"></div>
 		</div>
-		<div class="col-md-4"></div>
-	</div>
 
-	<div class="row frmLogin">
-		<div class="col-md-4"></div>
-		<div class="col-md-4">
-			<form role="form">
-				<div class="form-group">
-				<label for="login">Login</label><a href="" class="text-right"> Ainda não tenho uma conta</a>
-					<input type="email" class="form-control" id="login" placeholder="Utilize o login ou seu email">
-				</div>
-				<div class="form-group">
-					<label for="senha">Password</label>
-					<input type="password" class="form-control" id="senha" placeholder="Senha">
-				</div>
-				<button type="submit" class="btn btn-primary">Logar-se</button>
-				<a href="#" class="text-right esqSenha">esqueci minha senha</a>
-			</form>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4 frmCadastroUsuario">
+
+				<form role="form">
+					<div class="form-group">
+						<label for="nome">Nome completo</label>
+						<input type="text" class="form-control" name="nome" id="nome" placeholder="Nome completo">
+					</div>
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" class="form-control" name="email" id="email" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<label for="login">Login</label>
+						<input type="text" class="form-control" name="login" id="login" placeholder="Usuário de login (username)">
+					</div>
+					<div class="form-group">
+						<label for="senha">Senha</label>
+						<input type="password" class="form-control" name="senha" id="senha" placeholder="Senha">
+					</div>
+					<div class="form-group">
+						<label for="senha">Repita sua senha</label>
+						<input type="password" class="form-control" name="senha" id="senha" placeholder="Senha">
+					</div>
+					<div class="form-group">
+						<label for="foto">Imagem / Avatar</label>
+						<input type="file" id="foto">
+						<p class="help-block">Escolha uma foto para utilizar como avatar.</p>
+					</div>
+					<div class="checkbox">
+						<label>
+						<input type="checkbox"> <a href="#">Aceito os termos e condições do TCHEbooks.COM</a>
+						</label>
+					</div>
+					<button type="submit" class="btn btn-primary btnCadastrar">Cadastrar</button>
+				</form>
+
+			</div>
+			<div class="col-md-4"></div>
 		</div>
-		<div class="col-md-4"></div>
-	</div>
